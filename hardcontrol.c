7d9e79dbc8958e4ef68f4242ff9800c798392ba8
weidongshan <weidongshan@qq.com>
2015-08-18 19:35:28 +0800

    v1, empty functions for led operations

---
+++ hardcontrol.c
@@ -0,0 +1,60 @@
+
+#include <jni.h>  /* /usr/lib/jvm/java-1.7.0-openjdk-amd64/include/ */
+#include <stdio.h>
+#include <stdlib.h>
+ 
+#if 0
+typedef struct {
+    char *name;          /* Java里调用的函数名 */
+    char *signature;    /* JNI字段描述符, 用来表示Java里调用的函数的参数和返回值类型 */
+    void *fnPtr;          /* C语言实现的本地函数 */
+} JNINativeMethod;
+#endif
+
+jint ledOpen(JNIEnv *env, jobject cls)
+{
+	return 0;
+}
+
+void ledClose(JNIEnv *env, jobject cls)
+{
+}
+
+
+jint ledCtrl(JNIEnv *env, jobject cls, jint which, jint status)
+{
+	return 0;
+}
+
+
+static const JNINativeMethod methods[] = {
+	{"ledOpen", "()I", (void *)ledOpen},
+	{"ledClose", "()V", (void *)ledClose},
+	{"ledCtrl", "(II)I", (void *)ledCtrl},
+};
+
+
+
+
+/* System.loadLibrary */
+JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM *jvm, void *reserved)
+{
+	JNIEnv *env;
+	jclass cls;
+
+	if ((*jvm)->GetEnv(jvm, (void **)&env, JNI_VERSION_1_4)) {
+		return JNI_ERR; /* JNI version not supported */
+	}
+	cls = (*env)->FindClass(env, "com/thisway/hardlibrary/HardControl");
+	if (cls == NULL) {
+		return JNI_ERR;
+	}
+
+	/* 2. map java hello <-->c c_hello */
+	if ((*env)->RegisterNatives(env, cls, methods, sizeof(methods)/sizeof(methods[0])) < 0)
+		return JNI_ERR;
+
+	return JNI_VERSION_1_4;
+}
+
---
+++ readme.txt
@@ -0,0 +1,1 @@
+arm-linux-gcc -fPIC -shared hardcontrol.c -o libhardcontrol.so -I /usr/lib/jvm/java-1.7.0-openjdk-amd64/include/  -nostdlib /work/android-5.0.2/prebuilts/ndk/9/platforms/android-19/arch-arm/usr/lib/libc.so
