47cfc04c60b6d8f8ad803af87d5caaabfec81751
weidongshan <weidongshan@qq.com>
2015-08-19 17:53:33 +0800

    v3, call open,ioctl,close

--- hardcontrol.c
+++ hardcontrol.c
@@ -2,6 +2,10 @@
 #include <jni.h>  /* /usr/lib/jvm/java-1.7.0-openjdk-amd64/include/ */
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 
 #include <android/log.h>  /* liblog */
 
@@ -16,22 +20,30 @@ typedef struct {
 } JNINativeMethod;
 #endif
 
+static jint fd;
+
 jint ledOpen(JNIEnv *env, jobject cls)
 {
-	__android_log_print(ANDROID_LOG_DEBUG, "LEDDemo", "native ledOpen ...");
-	return 0;
+	fd = open("/dev/leds", O_RDWR);
+	__android_log_print(ANDROID_LOG_DEBUG, "LEDDemo", "native ledOpen : %d", fd);
+	if (fd >= 0)
+		return 0;
+	else
+		return -1;
 }
 
 void ledClose(JNIEnv *env, jobject cls)
 {
 	__android_log_print(ANDROID_LOG_DEBUG, "LEDDemo", "native ledClose ...");
+	close(fd);
 }
 
 
 jint ledCtrl(JNIEnv *env, jobject cls, jint which, jint status)
 {
-	__android_log_print(ANDROID_LOG_DEBUG, "LEDDemo", "native ledCtrl : %d, %d", which, status);
-	return 0;
+	int ret = ioctl(fd, status, which);
+	__android_log_print(ANDROID_LOG_DEBUG, "LEDDemo", "native ledCtrl : %d, %d, %d", which, status, ret);
+	return ret;
 }
 
 
